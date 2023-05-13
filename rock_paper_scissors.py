ce63649c799958cea58427acc00d46b0b5de23f2
Juan Ascanio <jd54am@hotmail.com>
2023-05-13 18:38:14 -0400

    Adds rock Paper Scissors Practice Program

---
+++ rock_paper_scissors.py
@@ -0,0 +1,65 @@
+import random, sys
+
+print('ROCK, PAPER, SCISSORS')
+
+wins = 0
+losses = 0
+ties = 0
+
+while True:
+    print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
+    while True: # Player input loop
+        playerMove = input('Enter your move: (r)ock (p)aper (s)cissors or (q)uit\n')
+        if playerMove == 'q':
+                sys.exit()
+        if playerMove == 'r' or playerMove == 'p' or playerMove == 's':
+            break
+        print('Type one of r, p, s, or q')
+
+    # Displays what the player chose
+    if playerMove == 'r':
+        print('Rock versus...')
+    elif playerMove == 'p':
+        print('Paper versus...')
+    elif playerMove == 's':
+        print('Scissors versus...')
+
+
+    # Makes the computer randomly choose a number to be assigned to a value
+    randomNumber = random.randint(1,3)
+    if randomNumber == 1:
+        computerMove = 'r'
+        print('ROCK')
+    elif randomNumber == 2:
+        computerMove = 'p'
+        print('PAPER')
+    elif randomNumber == 3:
+        computerMove = 's'
+        print('SCISSORS')
+
+    # Displays wins, losses, and ties and adds a value depending on the outcome
+    if playerMove == computerMove:
+        print('It is a tie!')
+        ties = ties + 1
+    elif playerMove == 'r' and computerMove == 'p':
+        print('You lose!')
+        losses = losses + 1
+    elif playerMove == 'r' and computerMove == 's':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 'p' and computerMove == 'r':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 'p' and computerMove == 's':
+        print('You lose!')
+        losses = losses + 1
+    elif playerMove == 's' and computerMove == 'p':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 's' and computerMove == 'r':
+        print('You lose!')
+        losses = losses + 1
+            
+            
+        
+    
